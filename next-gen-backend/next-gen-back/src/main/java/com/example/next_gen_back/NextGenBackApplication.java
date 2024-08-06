312a00e50e2419208dec7a73316ed5c4285f6f25
Eren Berk Eraslan <berk.eraslan@ug.bilkent.edu.tr>
2024-08-06 16:37:13 +0300

    Back End branch
    Built a spring boot maven project and created User model and Role enum

---
+++ next-gen-backend/next-gen-back/src/main/java/com/example/next_gen_back/NextGenBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.next_gen_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGenBackApplication.class, args);
+	}
+
+}
